d2bb34d0f3d1e9468bc91b7f2da6973ee071e4a5
Robin Vonk <robinvonk789@gmail.com>
2020-01-03 20:01:25 +0100

    Dataset feature update
    Now uses dict for storing data for simple clustering of categories.
    Now able to generate a dataset automatically by providing some google search terms related to the category.

--- dataset.py
+++ dataset.py
@@ -2,6 +2,7 @@ import os
 import cv2
 import random
 import numpy as np
+from google_images_download import google_images_download
 
 
 # TODO: Balance the dataset
@@ -10,9 +11,14 @@ class Dataset:
     Helps creating you a dataset to use for a neural network. It can do multiple things:
     Provide an path on the local system with images. This class will read them with Opencv, turn them to greyscale and
         resize them to the same dimensions. After this, they are ready to be used by Tensorflow.
+    This class can also generate a dataset it self. All you need to do is provide it with topics related to the category
+    It will use the topics to collect data from google images. You'll need to provide multiple topics, since the library
+    is only able to collect 100 images per topic at max.
     """
     IMAGE_SIZE = 100
-    MAX_BALANCE_DIFFERENCE = 5
+    MAX_DATA_BALANCE_DIFFERENCE = 5
+    DEFAULT_DATA_DIRECTORY = 'dataset/download/'
+    MAX_GOOGLE_IMAGES_PER_SEARCH = 100
 
     fileCounter = 0
 
@@ -23,14 +29,14 @@ class Dataset:
             add sources for the dataset for this category. This way you can add as many data sources as you want.
             Only accepts full paths.
             e.g: ['Dog', '/tmp/dogs/', 'X:/images/dogs/']
-        Data: The data from the sources, mapped by category in the following format:
-            [['Cat', data 1, data 2...],
-             ['Dog', data 1, data 2...],
+        Data: The data from the sources, mapped by category in a dictionary:
+            {'Cat' : [data 1, data 2...],
+             'Dog' : [data 1, data 2...],
              ...
-            ]
+            }
         """
         self.Categories = []
-        self.Data = []
+        self.Data = {}
 
     def add_category(self, cat):
         """
@@ -45,17 +51,21 @@ class Dataset:
         for category in self.Categories:
 
             name = category[0]
+            # Add the category to the dictionary with an empty list to store the data
+            self.Data[name] = []
+
             # Parse the data sources, skip the first item since that is the name of the category
             for source in category[1:]:
                 if source.startswith('/'):
                     self._run_filesystem(source, name)
-                elif source.startswith('http://') or source.startswith('https://'):
-                    if source.contains('www.google'):
-                        self._run_google_images()
-                    else:
-                        self._run_online()
+                elif ('/' not in source) and (':' not in source):
+                    print('Generating dataset of {}: {}'.format(category, source))
+                    self._run_google_images(source, name)
+                else:
+                    print('Could not understand source: {}'.format(source))
 
-                print('Parsed {} items for category {} from {}'.format(len(self.Data[-1]), name, source))
+                if len(self.Data):
+                    print('Parsed {} items for category {} from {}'.format(len(self.Data[name]), name, source))
 
     def normalize_data(self, data, max_value=255):
         """
@@ -79,7 +89,7 @@ class Dataset:
         return os.path.join(current_dir, url)
 
     def _run_filesystem(self, dir, category, normalize=True):
-        category_data = [category]
+        category_data = []
 
         for image in os.listdir(dir):
             try:
@@ -93,13 +103,18 @@ class Dataset:
             except Exception as e:
                 # Skip all broken images
                 print('Something went wrong with image {}: {}'.format(image, e))
-        self.Data.append(category_data)
+        self.Data[category] += category_data
 
-    def _run_online(self):
-        pass
+    def _run_google_images(self, topic, category):
+        response = google_images_download.googleimagesdownload()  # class instantiation
 
-    def _run_google_images(self):
-        pass
+        # Decided not to use a prefix in the file name. This way images shich appear at multiple searching will
+        # not be used multiple times in the dataset, but will just be overwritten
+        arguments = {'keywords': topic, 'limit': self.MAX_GOOGLE_IMAGES_PER_SEARCH, 'no_directory': True,
+                     'output_directory': self.DEFAULT_DATA_DIRECTORY}  # creating list of arguments
+        paths = response.download(arguments)  # passing the arguments to the function
+        self._run_filesystem(self.DEFAULT_DATA_DIRECTORY, category)
+
 
     def get_label_data_separated(self):
         """
@@ -110,9 +125,8 @@ class Dataset:
         labels = []
         datas = []
         for category in self.Data:
-            name = category[0]
-            for data in category[1:]:
-                labels.append(name)
+            for data in self.Data[category]:
+                labels.append(category)
                 datas.append(data)
         return labels, datas
 
@@ -124,9 +138,8 @@ class Dataset:
         labeled_data = []
         # Copy the data to the random data with labels as [category name, data][category name, data]...
         for category in self.Data:
-            name = category[0]
-            for data in category[1:]:
-                labeled_data.append([name, data])
+            for data in self.Data[category]:
+                labeled_data.append([category, data])
 
         return labeled_data
 
@@ -147,6 +160,7 @@ class Dataset:
         :param path: Path to the save location
         :return: None
         """
+        print('exporting dataset to {}'.format(path))
         np.save('{}data{}.npy'.format(path, Dataset.fileCounter), self.Data)
         Dataset.fileCounter += 1
 
@@ -156,6 +170,7 @@ class Dataset:
         :param path: Path to the save location
         :return: None
         """
+        print('exporting dataset to {}'.format(path))
         np.savez_compressed('{}data{}.npz'.format(path, Dataset.fileCounter), self.Data)
         Dataset.fileCounter += 1
 
@@ -166,13 +181,18 @@ class Dataset:
         :return: None
         """
         # Allow pickle to be able to import an array
-        self.Data = np.load(path, allow_pickle=True)
+        self.Data = np.load(path, allow_pickle=True).item()
+        self.Categories = self.Data.keys()
 
     def import_compressed(self, path, array_num='arr_0'):
-        self.Data = np.load(path, allow_pickle=True)[array_num]
+        self.Data = np.load(path, allow_pickle=True)[array_num].item()
+        self.Categories = self.Data.keys()
 
     def __str__(self):
         cats = ''
+        total = 0
         for cat in self.Data:
-            cats += '{}: {}\n'.format(cat[0], len(cat))
-        return 'Categories: {} \n Total data: {} \n {}'.format(self.Categories, len(self.Data), cats)
+            num = len(self.Data[cat])
+            cats += '{}: {}\n'.format(cat, num)
+            total += num
+        return 'Categories: {} \n Total data: {} \n {}'.format(self.Categories, total, cats)
--- example.py
+++ example.py
@@ -10,53 +10,58 @@ train_set = Dataset()
 
 # Add categories to the dataset with their sources. In this case we use the categories cats and dogs
 # and refer to images of cats and dogs on the filesystem
-cat_path = train_set.relative_to_absolute('dataset/cats_and_dogs/sCat/')
-dog_path = train_set.relative_to_absolute('dataset/cats_and_dogs/sDog/')
-cat = ['cat', cat_path]
-dog = ['dog', dog_path]
-train_set.add_category(cat)
-train_set.add_category(dog)
+# cat_path = train_set.relative_to_absolute('dataset/cats_and_dogs/sCat/')
+# dog_path = train_set.relative_to_absolute('dataset/cats_and_dogs/sDog/')
+# cat = ['cat', cat_path]
+# dog = ['dog', dog_path]
+# train_set.add_category(cat)
+# train_set.add_category(dog)
+
+# Add category to collect a dataset from google images
+train_set.add_category(['dog', 'dog', 'hond', 'puppy'])
+train_set.add_category(['cat', 'cat', 'kat', 'poes'])
 
 # Set the resolution of the images
-train_set.IMAGE_SIZE = 40
+train_set.IMAGE_SIZE = 4
 
 # Parse the data from the sources into the dataset
 train_set.run()
 
 # Export the dataset to a file, compressed or binary
-# train_set.export_bin(set.relative_to_absolute('dataset/'))
-# train_set.export_compressed(set.relative_to_absolute('dataset/s'))
+# train_set.export_bin(train_set.relative_to_absolute('dataset/s'))
+train_set.export_compressed(train_set.relative_to_absolute('dataset/s'))
 
 # Import a dataset from a file
-# train_set.import_bin(set.relative_to_absolute('dataset/data0.npy'))
-# train_set.import_compressed(set.relative_to_absolute('dataset/sdata0.npz'))
-
+# train_set.import_bin(train_set.relative_to_absolute('dataset/sdata0.npy'))
+# train_set.import_compressed(train_set.relative_to_absolute('dataset/sdata0.npz'))
+print(train_set)
 labels, data = train_set.get_label_data_separated()
-# npdata = np.reshape(np.asarray(data), (set.IMAGE_SIZE, set.IMAGE_SIZE))
+
+# npdata = np.reshape(np.asarray(data), (train_set.IMAGE_SIZE, train_set.IMAGE_SIZE))
 
 
 # ============ Learn ==============
 
-genie = Learner(labels, np.asarray(data))
-
-genie.EPOCHS = 10
-genie.train()
+# genie = Learner(labels, np.asarray(data))
+#
+# genie.EPOCHS = 10
+# genie.train()
 
 # ============ Test =============
 # Create the dataset
-test_set = Dataset()
+# test_set = Dataset()
 
 # Add categories to the dataset with their sources. In this case we use the categories cats and dogs
 # and refer to images of cats and dogs on the filesystem
-test_cat = train_set.relative_to_absolute('dataset/cats_and_dogs/tCat/')
-test_dog = train_set.relative_to_absolute('dataset/cats_and_dogs/tDog/')
-tcat = ['cat', test_cat]
-tdog = ['dog', test_dog]
-test_set.add_category(tcat)
-test_set.add_category(tdog)
-
-test_set.run()
-da, ta = test_set.get_label_data_separated()
-print(da)
-print(ta)
-genie.test(da, ta)
+# test_cat = train_set.relative_to_absolute('dataset/cats_and_dogs/tCat/')
+# test_dog = train_set.relative_to_absolute('dataset/cats_and_dogs/tDog/')
+# tcat = ['cat', test_cat]
+# tdog = ['dog', test_dog]
+# test_set.add_category(tcat)
+# test_set.add_category(tdog)
+#
+# test_set.run()
+# da, ta = test_set.get_label_data_separated()
+# print(da)
+# print(ta)
+# genie.test(da, ta)
